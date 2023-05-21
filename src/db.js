7bf67660be4382dbb83dbfd7bb6dc582f84aef88
Mário Santos <mariodev7@gmail.com>
2023-05-21 14:56:29 -0300

    fix database insert method

--- src/db.js
+++ src/db.js
@@ -29,8 +29,8 @@ export class Database {
   }
 
   insert(table, data) {
-    if (Array.isArray(data)) {
-      this.#database[table] = data;
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
     } else {
       this.#database[table] = [data];
     }
