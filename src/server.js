6f3fb5421ab49097d684df4e3e627c26cf901e8e
Mário Santos <mariodev7@gmail.com>
2023-05-20 21:55:34 -0300

    create simple routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Create user");
+  }
+
+  if (method === "GET" && url === "/users") {
+    return res.end("List users");
+  }
+
   return res.end("Hello Ignite");
 });
 
