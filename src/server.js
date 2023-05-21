017fde5ca0c4821abdea2e77344e2438b93996f9
Mário Santos <mariodev7@gmail.com>
2023-05-20 22:00:49 -0300

    save users in a variable

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
     return res.end("Create user");
   }
 
   if (method === "GET" && url === "/users") {
-    return res.end("List users");
+    return res.end(JSON.stringify(users));
   }
 
   return res.end("Hello Ignite");
